1d208b24f93b5bfcfe13d48950223ac5ebace040
BarsadeJesus <pattycbs28@gmail.com>
2020-03-26 15:53:50 -0500

    signo mas y menos en la orden

--- src/components/ItemOrder.jsx
+++ src/components/ItemOrder.jsx
@@ -12,7 +12,9 @@ const ItemOrder = ({ data, delOrderProduct }) => {
     
     return (
     <li className='menu-item-list-order'>
-         <p className='numbers' data-testid='item'> { Cantidad }</p>
+    <button className='less1' ><img src="less.jpg" className='less' alt='less'/></button>
+    <p className='numbers' data-testid='item'> { Cantidad }</p>
+    <button className='more' ><img src="plus.png" alt='plus'/></button>
         <p className='elements' data-testid='item'>{ Descripcion }</p>
         <p className='numbers' data-testid='item'> { Precio }</p>
         <button onClick={btnEliminarItem} className='numbers' >
