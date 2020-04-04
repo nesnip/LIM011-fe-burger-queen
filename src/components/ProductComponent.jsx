c07b381d3e461f940f625d3231dba5174d0c9757
BarsadeJesus <pattycbs28@gmail.com>
2020-04-03 20:01:57 -0500

    Botones mas y menos

--- src/components/ProductComponent.jsx
+++ src/components/ProductComponent.jsx
@@ -5,11 +5,24 @@ const ProductComponent = ({ dataDeUnProducto, addProductsList, totClient }) => {
   const addProductItem = addProductsList;
   const totalCta = totClient;
   const obj1 = dataDeUnProducto;
-  const [cant, setCant]= useState('')
-  function handleChange(event) {
+  const [quant, setQuant] = useState(1);
+
+  const moreQuantity = () => {
+    //event.preventDefault();
+    setQuant(quant + 1);
+   
+}
+const lessQuantity = () => {
+    //event.preventDefault();
+    setQuant(quant - 1);
+    /* const tP= quant * Precio;
+    setTotal(tP);
+    console.log(total); */
+}
+  /* function handleChange(event) {
     const valor= event.target.value;
     setCant(valor);
-  };
+  }; */
   
   function addItemProduct (id, desc, price, quantity) {  
     if (quantity === '' ) {
@@ -28,7 +41,7 @@ const ProductComponent = ({ dataDeUnProducto, addProductsList, totClient }) => {
   let totalCount = 0;
   const buttonTotal = (objTotal) => {
     const price= objTotal.Precio;
-    const quantity = cant;
+    const quantity = quant;
     totalCount += (price * quantity);
     totalCta(totalCount); 
   };
@@ -37,16 +50,16 @@ const element = (
   <li className='menu-item-list' >
     <p data-testId='idItem' className='elements' >{obj1.Descripcion}</p>
     <p data-testId='idItem' className='numbers' >S/.{obj1.Precio}</p>
-    <p data-testId='idItem' className='numbers'>
-      <input data-testId='idInput' className='quant-prod' type='text' value={cant} onChange={handleChange}></input>
-    </p>
+    <button className='less1' onClick = {lessQuantity}><img src="less.jpg" className='less' alt='less'/></button>
+    <p className='numbers' data-testid='item'> { quant }</p>
+    <button className='more' onClick = {moreQuantity}><img src="plus.png" alt='plus'/></button>
     <p className='numbers'>
       <button className='more' onClick={(event)=> {
         event.preventDefault();
-        const valor1 = cant;
+        const valor1 = quant;
         addItemProduct(obj1.id, obj1.Descripcion, obj1.Precio, valor1);
         buttonTotal(obj1);
-        }}><img src="plus.png" alt='plus'/> 
+        }}><img src="flecha.png" className='arrow' alt='arrow'/> 
       </button>
     </p>
   </li>
