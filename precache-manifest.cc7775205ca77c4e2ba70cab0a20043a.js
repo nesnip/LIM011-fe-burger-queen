02d0dc9e516271a53af21569604ef3f67c38ce54
BarsadeJesus <pattycbs28@gmail.com>
2020-06-03 18:11:48 -0500

    Updates

---
+++ precache-manifest.cc7775205ca77c4e2ba70cab0a20043a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4de7c32dc3939b091cb9626fe73b2a5",
+    "url": "/LIM011-fe-burger-queen/index.html"
+  },
+  {
+    "revision": "5481afa4e7bc0ad398a0",
+    "url": "/LIM011-fe-burger-queen/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "413c923eee4235e20347",
+    "url": "/LIM011-fe-burger-queen/static/css/main.a9aa8fc0.chunk.css"
+  },
+  {
+    "revision": "5481afa4e7bc0ad398a0",
+    "url": "/LIM011-fe-burger-queen/static/js/2.246d9626.chunk.js"
+  },
+  {
+    "revision": "f73e810844fba009bd106bf78cf93a52",
+    "url": "/LIM011-fe-burger-queen/static/js/2.246d9626.chunk.js.LICENSE"
+  },
+  {
+    "revision": "413c923eee4235e20347",
+    "url": "/LIM011-fe-burger-queen/static/js/main.efdf1d24.chunk.js"
+  },
+  {
+    "revision": "d589a421a9a7c11eff68",
+    "url": "/LIM011-fe-burger-queen/static/js/runtime-main.8ccf3e6d.js"
+  }
+]);
