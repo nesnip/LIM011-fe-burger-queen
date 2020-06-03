9b155385ea210ae7315581acd35942af67a39632
BarsadeJesus <pattycbs28@gmail.com>
2020-06-03 18:34:15 -0500

    Updates

--- precache-manifest.763065d4013db569a3f90f73e2adc241.js
+++ precache-manifest.763065d4013db569a3f90f73e2adc241.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e4de7c32dc3939b091cb9626fe73b2a5",
+    "revision": "9b2ffab06b2467d390201e2eeecb4492",
     "url": "/LIM011-fe-burger-queen/index.html"
   },
   {
-    "revision": "5481afa4e7bc0ad398a0",
+    "revision": "d14a01a1d686df0a4177",
     "url": "/LIM011-fe-burger-queen/static/css/2.67123b20.chunk.css"
   },
   {
-    "revision": "413c923eee4235e20347",
+    "revision": "c42a9d5e00d40032b2cc",
     "url": "/LIM011-fe-burger-queen/static/css/main.a9aa8fc0.chunk.css"
   },
   {
-    "revision": "5481afa4e7bc0ad398a0",
-    "url": "/LIM011-fe-burger-queen/static/js/2.246d9626.chunk.js"
+    "revision": "d14a01a1d686df0a4177",
+    "url": "/LIM011-fe-burger-queen/static/js/2.f0fc856a.chunk.js"
   },
   {
     "revision": "f73e810844fba009bd106bf78cf93a52",
-    "url": "/LIM011-fe-burger-queen/static/js/2.246d9626.chunk.js.LICENSE"
+    "url": "/LIM011-fe-burger-queen/static/js/2.f0fc856a.chunk.js.LICENSE"
   },
   {
-    "revision": "413c923eee4235e20347",
-    "url": "/LIM011-fe-burger-queen/static/js/main.efdf1d24.chunk.js"
+    "revision": "c42a9d5e00d40032b2cc",
+    "url": "/LIM011-fe-burger-queen/static/js/main.43b7eb09.chunk.js"
   },
   {
     "revision": "d589a421a9a7c11eff68",
