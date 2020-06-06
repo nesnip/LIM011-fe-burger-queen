d551012a5521addf6cbb21add43fa60004d53400
nesnip <hpmarines12@gmail.com>
2020-06-06 04:14:51 -0500

    Updates

--- precache-manifest.94bc6010b518d19abc6d56cfe9917ab5.js
+++ precache-manifest.94bc6010b518d19abc6d56cfe9917ab5.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6dd7115e702d54c489d0eb777bdc4866",
+    "revision": "7af130b2d1240e65335b0234145985a6",
     "url": "/LIM011-fe-burger-queen/index.html"
   },
   {
-    "revision": "5481afa4e7bc0ad398a0",
+    "revision": "d14a01a1d686df0a4177",
     "url": "/LIM011-fe-burger-queen/static/css/2.67123b20.chunk.css"
   },
   {
-    "revision": "433f48ca70eb2575158b",
+    "revision": "1b256cb86683c630d12f",
     "url": "/LIM011-fe-burger-queen/static/css/main.71c2d77a.chunk.css"
   },
   {
-    "revision": "5481afa4e7bc0ad398a0",
-    "url": "/LIM011-fe-burger-queen/static/js/2.246d9626.chunk.js"
+    "revision": "d14a01a1d686df0a4177",
+    "url": "/LIM011-fe-burger-queen/static/js/2.f0fc856a.chunk.js"
   },
   {
     "revision": "f73e810844fba009bd106bf78cf93a52",
-    "url": "/LIM011-fe-burger-queen/static/js/2.246d9626.chunk.js.LICENSE"
+    "url": "/LIM011-fe-burger-queen/static/js/2.f0fc856a.chunk.js.LICENSE"
   },
   {
-    "revision": "433f48ca70eb2575158b",
-    "url": "/LIM011-fe-burger-queen/static/js/main.73854b0c.chunk.js"
+    "revision": "1b256cb86683c630d12f",
+    "url": "/LIM011-fe-burger-queen/static/js/main.009f13a0.chunk.js"
   },
   {
     "revision": "d589a421a9a7c11eff68",
