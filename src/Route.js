64fc851ea1c0a01bf8c70a6ef007dc6e07fa492f
nesnip <hpmarines12@gmail.com>
2020-06-06 04:16:05 -0500

    update

--- src/Route.js
+++ src/Route.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, Route, BrowserRouter} from 'react-router-dom';
+import {Switch, Route, HashRouter} from 'react-router-dom';
 import OrderView from './Views/Order-View';
 import KitchenView from './Views/Kitchen-View';
 import ReadyView from './Views/Ready-View';
@@ -7,14 +7,14 @@ import ReadyView from './Views/Ready-View';
 
 const Routes = () => {
     return (
-      <BrowserRouter>
+      <HashRouter>
         <Switch>
           <Route exact path='/' component={OrderView} />
           <Route exact path='/OrderView' component={OrderView} />
           <Route exact path='/KitchenView' component={KitchenView} />
           <Route exact path='/ReadyView' component={ReadyView} />
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     )
 }
 export default Routes;
